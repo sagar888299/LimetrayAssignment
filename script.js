d73acb5a252f751f674086d78a00e98535ef0f6b
Sagar <sagar888299@gmail.com>
2025-05-28 17:12:31 +0530

    resposiveness

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ const nextButton = document.querySelector(".next");
 const leftButton = document.querySelector(".prev");
 const slider = document.querySelector(".slides");
 nextButton.addEventListener("click", function () {
-  slider.scrollBy({ left: 1260, behavior: "smooth" });
+  slider.scrollBy({ left: slider.scrollWidth/3, behavior: "smooth" });
 });
 leftButton.addEventListener("click", function () {
-  slider.scrollBy({ left: -1260, behavior: "smooth" });
+  slider.scrollBy({ left: slider.scrollWidth/3, behavior: "smooth" });
 });
 
 setInterval(() => {
     if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
       slider.scrollTo({ left: 0, behavior: "smooth" });
     } else {
-      slider.scrollBy({ left: 1260, behavior: "smooth" });
+      slider.scrollBy({ left: slider.scrollWidth/3, behavior: "smooth" });
     }
   }, 3000);
+
+  const toggle = document.getElementById('menu-toggle');
+  const navLinks = document.querySelector('.navbar-links');
+
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
